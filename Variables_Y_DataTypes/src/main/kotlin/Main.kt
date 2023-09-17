915a7b601458ad41bf592c4c2e9f08b7bd216bec
Edson Cabrera Acevedo <144571624+EdCabrera02@users.noreply.github.com>
2023-09-17 17:54:35 -0600

    Add files via upload

---
+++ Variables_Y_DataTypes/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+const val PI = 3.1416F
+
+fun main() {
+    val decimal = 7f
+    println(decimal.javaClass.kotlin)
+
+    val Resultado = 2 * PI * decimal
+
+    println("El perímetro del círculo: $Resultado")
+}

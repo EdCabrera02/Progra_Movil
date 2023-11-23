0b3ceeae499ec8a754c50a3a394570e3350f774e
Edson Cabrera Acevedo <144571624+EdCabrera02@users.noreply.github.com>
2023-11-22 18:39:12 -0600

    Add files via upload

---
+++ Vehiculos/src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+import Clases.Vehiculos
+
+fun main(){
+
+        //Clase Vehiculo
+        val miVehiculo = Vehiculos("Ford","2020","Verde")
+        miVehiculo.placas = "NNT3047"
+        println("El coche está encendido? ${miVehiculo.encendido}")
+        miVehiculo.encender()
+        println("El coche encendio? ${miVehiculo.encendido}")
+        println("El tanque tiene ${miVehiculo.gasolina}")
+        miVehiculo.recargar(20.07f)
+        println("El tanque ahora tiene ${miVehiculo.gasolina}")
+        val golNegro = Vehiculos("Volkswagen", "Gol")
+        //NamedArguments
+        val sonicAzul = Vehiculos(
+                marca = "Ford",
+                modelo = "Sonic",
+                color = "Azul",
+                placas = "ALS9763"
+        )
+        println(golNegro)
+        println(sonicAzul)
+    }
+
+
